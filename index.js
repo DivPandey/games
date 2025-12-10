6d6df418ef8522adbf601dc0aad4ee8b034ebece
JATIN CHULET <2024EB02230@online.bits-pilani.ac.in>
2025-12-10 16:03:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
     function playBreathing() {
-      window.location.href = "breathingGame/breathing.html"; 
+      window.location.href = "./breathingGame/breathing.html"; 
     }
 
     function playRaisin() {
-      window.location.href = "raisinIntro.html"; 
+      window.location.href = "./raisinIntro.html"; 
     }
 
     function playBoth() {
       localStorage.setItem("playMode", "both");
-      window.location.href = "breathingGame/breathing.html"; 
+      window.location.href = "./breathingGame/breathing.html"; 
     }
